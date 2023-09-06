da531761739137d8e3df02fcaf18fadfd2038f08
HerbertyFreire <134555045+HerbertyFreire@users.noreply.github.com>
2023-09-05 21:23:27 -0300

    Create Média 2.c

---
+++ Média 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float a,b,c;
+    scanf("%f%f%f", &a,&b,&c);
+    
+    float media ;
+    
+     
+    media = (a * 2 + b * 3 + c * 5) / (2 + 3 + 5);
+    
+     printf("MEDIA = %.1f\n",media);
+     
+    
+    
+	return 0;
+}
