ecb889d5efe43fbabfb8d84f596dd359b28c4534
HerbertyFreire <134555045+HerbertyFreire@users.noreply.github.com>
2023-09-05 21:21:29 -0300

    Create Extremamente Básico.c

---
+++ Extremamente Básico.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,S;
+    scanf("%d%d",&a, &b);
+    
+    S = (a+b);
+    
+    printf("X = %d\n",S);
+    
+	return 0;
+}
