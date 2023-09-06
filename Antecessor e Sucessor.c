a88e378c1798b7b4d28150ad38b36a9ba13a67da
HerbertyFreire <134555045+HerbertyFreire@users.noreply.github.com>
2023-09-05 21:16:13 -0300

    Update Antecessor e Sucessor.c

--- Antecessor e Sucessor.c
+++ Antecessor e Sucessor.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main() {
+    int numero, antecessor, sucessor;
 
+    printf(" ");
+    scanf("%d", &numero);
+
+    antecessor = numero - 1;
+    sucessor = numero + 1;
+
+    printf("%d %d\n", antecessor, sucessor);
+
+    return 0;
+}
