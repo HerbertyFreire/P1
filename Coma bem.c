87965dab400a31028a69288f3010ea4e22655be8
HerbertyFreire <134555045+HerbertyFreire@users.noreply.github.com>
2023-09-05 21:20:27 -0300

    Create Coma bem.c

---
+++ Coma bem.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float x,p;
+    scanf("%f",&x);
+
+p = x + (x * 0.1 );
+
+    printf("%.2f\n",p);
+
+    
+	return 0;
+}
