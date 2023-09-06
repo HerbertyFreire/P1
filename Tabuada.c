1842bd9b68363651b3c4da0f7bb85225ef110c98
HerbertyFreire <134555045+HerbertyFreire@users.noreply.github.com>
2023-09-05 21:23:02 -0300

    Create Tabuada.c

---
+++ Tabuada.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+   
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    double n = x;
+    
+    printf("%d X 1 = %.lf\n", x, n * 1);
+    printf("%d X 2 = %.lf\n", x, n * 2);
+    printf("%d X 3 = %.lf\n", x, n * 3);
+    printf("%d X 4 = %.lf\n", x, n * 4);
+    printf("%d X 5 = %.lf\n", x, n * 5);
+    printf("%d X 6 = %.lf\n", x, n * 6);
+    printf("%d X 7 = %.lf\n", x, n * 7);
+    printf("%d X 8 = %.lf\n", x, n * 8);
+    printf("%d X 9 = %.lf\n", x, n * 9);
+    
+    return 0;
+}
