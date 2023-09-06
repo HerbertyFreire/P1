f38b45b289a358283fbc0316bb4eeee37f6e90d4
HerbertyFreire <134555045+HerbertyFreire@users.noreply.github.com>
2023-09-05 21:18:13 -0300

    Update and rename Diferença to Diferença.c

---
+++ Diferença.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,c,d;
+    int D;
+    scanf("%d%d%d%d", &a, &b,&c,&d);
+    
+  D =  (a * b) - (c * d);
+      
+      printf("DIFERENCA = %d\n",D);
+    
+    
+    
+    
+	return 0;
+}
